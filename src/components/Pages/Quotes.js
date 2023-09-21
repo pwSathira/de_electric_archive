e05ab43182e897ad197581d05b03f19a93bc4be8
Sathira Williams <sathira.williams@gmail.com>
2023-09-21 15:16:24 -0400

    (Addition) Added support for Netlify Forms.

--- src/components/Pages/Quotes.js
+++ src/components/Pages/Quotes.js
@@ -14,7 +14,7 @@ export const Quotes = () => {
         <div className='MainContainer'>
             <h1 className="Heading2">Request a Quote</h1>
             <div className='QuotesContainer'>
-                <form className='FormClass'>
+                <form className='FormClass' netlify>
                     <div>
                         <input className="qInput" type="text" id="name" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
                     </div>
