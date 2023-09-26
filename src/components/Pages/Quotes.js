ffeea32026063dad88303dc90739d0bfee247fe9
Sathira Williams <sathira.williams@gmail.com>
2023-09-26 10:54:09 -0400

    (Fix) Support for Netlify Forms redone.

--- src/components/Pages/Quotes.js
+++ src/components/Pages/Quotes.js
@@ -15,23 +15,26 @@ export const Quotes = () => {
         <div className='MainContainer'>
             <h1 className="Heading2">Request a Quote</h1>
             <div className='QuotesContainer'>
-                <form className='FormClass' method="POST" data-netlify="true" name="quoteForm">
-                    {/* This hidden input field is essential for Netlify to recognize the form */}
-                    <input type="hidden" name="form-name" value="quoteForm" />
 
+                {/* This is the hidden pure HTML form for Netlify */}
+                <form name="quoteForm" data-netlify="true" netlify-honeypot="bot-field" hidden>
+                    <input type="text" name="name" />
+                    <input type="email" name="email" />
+                    <textarea name="message"></textarea>
+                </form>
+
+                {/* This is the React form for users */}
+                <form className='FormClass' method="POST" data-netlify="true" onSubmit={handleSubmit}>
                     <div>
                         <input className="qInput" type="text" id="name" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
                     </div>
-
                     <div>
                         <input className="qInput" type="email" id="email" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
                     </div>
-
                     <div>
                         <textarea className="qMessage" id="message" name="message" value={message} placeholder="Message" onChange={e => setMessage(e.target.value)} />
                     </div>
-
-                    <button className="qButton" type="submit" onClick={handleSubmit}>Submit</button>
+                    <button className="qButton" type="submit">Submit</button>
                 </form>
             </div>
         </div>
