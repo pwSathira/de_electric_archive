b78c58bc2fd0f86c353e5c4d5c2525864daa8522
Sathira Williams <sathira.williams@gmail.com>
2023-09-26 10:50:31 -0400

    (Fix) Support for Netlify Forms redone.

--- src/components/Pages/Quotes.js
+++ src/components/Pages/Quotes.js
@@ -1,37 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./Pages.css";
 
 export const Quotes = () => {
-    const [ name, setName ] = useState('')
-    const [ email, setEmail ] = useState('')
-    const [ message, setMessage ] = useState('')
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
     function handleSubmit(e) {
-        e.preventDefault()
-        console.log(name, email, message)
+        e.preventDefault();
+        console.log(name, email, message);
     }
 
     return (
         <div className='MainContainer'>
             <h1 className="Heading2">Request a Quote</h1>
             <div className='QuotesContainer'>
-                <form className='FormClass' method="POST" netlify>
+                <form className='FormClass' method="POST" data-netlify="true" name="quoteForm">
+                    {/* This hidden input field is essential for Netlify to recognize the form */}
+                    <input type="hidden" name="form-name" value="quoteForm" />
+
                     <div>
-                        <input className="qInput" type="text" id="name" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
+                        <input className="qInput" type="text" id="name" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
                     </div>
 
                     <div>
-                        <input className="qInput" type="email" id="email" name="email" placeholder="Email"  value={email} onChange={e => setEmail(e.target.value)}/>
+                        <input className="qInput" type="email" id="email" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
                     </div>
 
                     <div>
-                        <textarea className="qMessage" id="message" name="message" value={message} placeholder="Message" onChange={e => setMessage(e.target.value)}/>
+                        <textarea className="qMessage" id="message" name="message" value={message} placeholder="Message" onChange={e => setMessage(e.target.value)} />
                     </div>
 
                     <button className="qButton" type="submit" onClick={handleSubmit}>Submit</button>
                 </form>
             </div>
         </div>
-
     );
 };
-
