9f9abd8887c19937e31e144e9a456c24500467ef
Sathira Williams <sathira.williams@gmail.com>
2023-10-12 23:29:25 -0400

    (Add) Added Homepage, with hero banner.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "./Pages.css";
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+    const navigate = useNavigate();
+
+    function handleButtonClick() {
+        navigate('/quotes');
+    }
+
     return (
-        <div>
-            <h1 className="MainContainer">Home</h1>
+        <div className="MainContainerALT">
+            <div className="Hero-Image">
+                <div className="Hero-Text">
+                    <div className="TextContainer">
+                        <h1 className="Heading3">Dnipro Electric Inc.</h1>
+                        <h1 className="Heading4">Powering your future</h1>
+                        <button className="qButton" onClick={handleButtonClick}>Get Quoted</button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="HomeContainer">
+                <div className="col">
+                    {/*<h1 className="Heading1">1</h1>*/}
+                    {/*<div className="DPE_CS"></div>*/}
+                </div>
+                <div className="col">
+                    {/*<h1 className="Heading1">2</h1>*/}
+                    <img src="  HomeImages/DPE_Commercial.png" alt="Commercial"></img>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Quotes.js
+++ src/components/Pages/Quotes.js
@@ -1,16 +1,36 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./Pages.css";
+
 
 export const Quotes = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("https://formspree.io/f/meqbrbyq", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name, email, message })
+        }).then(response => {
+            if (response.ok) {
+                navigate('/'); // Redirect to homepage
+            } else {
+                console.error("Failed to submit form");
+            }
+        });
+    }
 
     return (
         <div className='MainContainer'>
             <h1 className="Heading2">Request a Quote</h1>
             <div className='QuotesContainer'>
-                <form className='FormClass' method="POST" action="https://formspree.io/f/meqbrbyq">
+                <form className='FormClass' onSubmit={handleSubmit}>
                     <div>
                         <input className="qInput" type="text" id="name" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
                     </div>
